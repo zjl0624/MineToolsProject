18211bdb80d4a6d61e6ad98424396d5304954b03
zjl <369907659@qq.com>
2018-02-26 17:03:17 +0800

    增加了网络请求，回调进度

--- ZJLFaceProject/NetManger.m
+++ ZJLFaceProject/NetManger.m
@@ -21,15 +21,21 @@ static id _instance;
 }
 
 - (void)initManager {
-	_manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:BaseUrl]];
+	_manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@""]];
 }
 - (void)getWithUrl:(NSString *)url param:(NSDictionary *)param success:(void(^)(id response))success  {
 //	NSString *completeUrl = [NSString stringWithFormat:@"%@",url];
-	[_manager GET:url parameters:param progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+	[_manager GET:@"http://cc.cocimg.com/api/uploads/20160322/1458633174180748.png" parameters:param progress:^(NSProgress * _Nonnull downloadProgress) {
+		float x = (float)downloadProgress.completedUnitCount / (float)downloadProgress.totalUnitCount * 100;
+		NSMutableString *str = [[NSMutableString alloc] initWithString:[NSString stringWithFormat:@"%.2f",x]];
+		[str appendString:@"%"];
+		NSLog(@"%@",str);
+	} success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
 		success(responseObject);
 	} failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
 		NSLog(@"%@",error.description);
 	}];
+	
 }
 
 - (void)postWithUrl:(NSString *)url param:(NSString *)param success:(void(^)(id response))success failure:(void(^)(NSString *errMsg))failure{
@@ -38,5 +44,7 @@ static id _instance;
 	} failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
 		failure(error.description);
 	}];
+	
+	
 }
 @end
