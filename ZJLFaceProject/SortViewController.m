a16fe696278779fa453de07a353121dd08d9541d
zjl <369907659@qq.com>
2018-07-25 15:11:47 +0800

    实现快速排序和选择排序

--- ZJLFaceProject/SortViewController.m
+++ ZJLFaceProject/SortViewController.m
@@ -9,7 +9,7 @@
 #import "SortViewController.h"
 
 @interface SortViewController ()
-@property (nonatomic,strong) UILabel *arrLabel;
+@property (nonatomic,strong) UITextView *arrLabel;
 
 @property (nonatomic,strong) UIButton *bubbleSortBtn;
 
@@ -18,6 +18,7 @@
 @property (nonatomic,strong) UIButton *quickSortBtn;
 
 @property (nonatomic,strong) NSMutableArray *dataArray;
+
 @end
 
 @implementation SortViewController
@@ -25,10 +26,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 	self.view.backgroundColor = [UIColor whiteColor];
-	_arrLabel = [[UILabel alloc] init];
-	_arrLabel.frame = CGRectMake(10, 88, ScreenWidth, 30);
-	_arrLabel.text = @"2,5,3,6,11,56,33,22,45,111,222,123,7";
-	_dataArray = [NSMutableArray arrayWithArray:[_arrLabel.text componentsSeparatedByString:@","]];
+	_arrLabel = [[UITextView alloc] init];
+	_arrLabel.frame = CGRectMake(10, 88, ScreenWidth - 20, 200);
+	_arrLabel.font = [UIFont systemFontOfSize:18];
+	_dataArray = [[NSMutableArray alloc] init];
+	[self getRandomNum:10];
+	_arrLabel.text = [_dataArray componentsJoinedByString:@","];
+
 	[self.view addSubview:_arrLabel];
 	
 	_bubbleSortBtn = [UIButton buttonWithType:UIButtonTypeSystem];
@@ -44,7 +48,7 @@
 	[self.view addSubview:_selectionSortBtn];
 	
 	_quickSortBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-	[_quickSortBtn setTitle:@"选择排序" forState:UIControlStateNormal];
+	[_quickSortBtn setTitle:@"快速排序" forState:UIControlStateNormal];
 	_quickSortBtn.frame = CGRectMake(CGRectGetMaxX(_selectionSortBtn.frame) + 10, CGRectGetMaxY(_arrLabel.frame) + 10, 80, 30);
 	[_quickSortBtn addTarget:self action:@selector(quickSortBtnClick:) forControlEvents:UIControlEventTouchUpInside];
 	[self.view addSubview:_quickSortBtn];
@@ -74,14 +78,57 @@
 }
 
 - (void)selectionSortBtnClick:(UIButton *)sender {
-	for (int i = 0; i < _dataArray.count; i++) {
-//		NSInteger min = i;
-		
+	for (NSInteger i = 0; i < _dataArray.count; i++) {
+		NSInteger minIndex = i;
+		NSNumber *min = _dataArray[i];
+		for (NSInteger j = i; j < _dataArray.count; j++) {
+			if (_dataArray[j] < min) {
+				minIndex = j;
+				min = _dataArray[j];
+			}
+		}
+		NSNumber *temp = _dataArray[i];
+		_dataArray[i] = _dataArray[minIndex];
+		_dataArray[minIndex] = temp;
 	}
+	_arrLabel.text = [_dataArray componentsJoinedByString:@","];
 }
 
 
 - (void)quickSortBtnClick:(UIButton *)sender {
-	
+	[self quickSortWithLeft:0 right:_dataArray.count - 1];
+	_arrLabel.text = [_dataArray componentsJoinedByString:@","];
+}
+
+- (void)quickSortWithLeft:(NSInteger)left right:(NSInteger)right {
+	if (left > right) {
+		return;
+	}
+	NSInteger temp = [_dataArray[left] integerValue];
+	NSInteger i = left;
+	NSInteger j = right;
+	for (j = right; j > i; j--) {
+		if ([_dataArray[j] integerValue] < temp) {
+			for (i = i; i < j; i++) {
+				if ([_dataArray[i] integerValue] > temp) {
+					NSNumber *x = _dataArray[i];
+					_dataArray[i] = _dataArray[j];
+					_dataArray[j] = x;
+					break;
+				}
+			}
+		}
+	}
+	_dataArray[left] = _dataArray[i];
+	_dataArray[i] = @(temp);
+	[self quickSortWithLeft:left right:i - 1];
+	[self quickSortWithLeft:i + 1 right:right];
+}
+- (void)getRandomNum:(NSInteger)n {
+	[_dataArray removeAllObjects];
+	for (int i = 0; i < n; i++) {
+		NSNumber *num = [NSNumber numberWithInt:(arc4random() % 100) + 1];
+		[_dataArray addObject:num];
+	}
 }
 @end
