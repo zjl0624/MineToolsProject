3c9bd1fd802ed728f13b04b4e9e2648d4425542c
zjl <369907659@qq.com>
2019-04-26 15:46:26 +0800

    使侧边栏盖住导航栏

--- ZJLFaceProject/SideBar/SideBarViewController.m
+++ ZJLFaceProject/SideBar/SideBarViewController.m
@@ -8,8 +8,10 @@
 
 #import "SideBarViewController.h"
 #import "SideBar.h"
-
+#import "AppDelegate.h"
 @interface SideBarViewController ()
+@property (nonatomic,strong) UILabel *testLabel;
+@property (nonatomic,strong) UIView *shadowView;
 @property (nonatomic,strong) SideBar *sideBar;
 @property (nonatomic,strong) UIPanGestureRecognizer *panGesture;
 @end
@@ -20,15 +22,27 @@
     [super viewDidLoad];
     self.navigationController.interactivePopGestureRecognizer.enabled = NO;
     self.view.backgroundColor = [UIColor whiteColor];
+    _testLabel = [[UILabel alloc] init];
+    _testLabel.text= @"测试";
+    _testLabel.textColor = [UIColor blackColor];
+    _testLabel.frame = CGRectMake(0, 100, 100, 30);
+    [self.view addSubview:_testLabel];
 
 }
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     if (!_sideBar) {
         _sideBar = [[SideBar alloc] initWithFrame:CGRectMake(-200, 0, 200, [UIScreen mainScreen].bounds.size.height)];
-        [self.view addSubview:_sideBar];
+        [[UIApplication sharedApplication].delegate.window addSubview:_sideBar];
         _panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
         [self.view addGestureRecognizer:_panGesture];
+        _shadowView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+        _shadowView.backgroundColor = [UIColor blackColor];
+        _shadowView.alpha = 0.4;
+        _shadowView.hidden = YES;
+        [[UIApplication sharedApplication].delegate.window addSubview:_shadowView];
+        UIPanGestureRecognizer *panGesture1 = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+        [_shadowView addGestureRecognizer:panGesture1];
     }
 }
 
@@ -38,23 +52,31 @@
 }
 
 - (void)pan:(UIPanGestureRecognizer *)panGes {
-    CGPoint p = [_panGesture translationInView:self.view];
+    CGPoint p = [panGes translationInView:self.view];
     NSLog(@"%f",p.x);
     if (panGes.state == 2) {
         if (CGRectGetMinX(self.view.frame) + p.x >= 0 && CGRectGetMinX(self.view.frame) + p.x <= CGRectGetWidth(_sideBar.frame)) {
             self.view.frame = CGRectMake(CGRectGetMinX(self.view.frame) + p.x, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
             [panGes setTranslation:CGPointZero inView:self.view];
+            _sideBar.frame = CGRectMake(CGRectGetMinX(_sideBar.frame) + p.x, 0, CGRectGetWidth(_sideBar.frame), CGRectGetHeight(_sideBar.frame));
+            _shadowView.hidden = NO;
+            _shadowView.frame = CGRectMake(CGRectGetMaxX(_sideBar.frame), 0, CGRectGetWidth(_shadowView.frame), CGRectGetHeight(_shadowView.frame));
         }
 
     }else if (panGes.state == 3){
         if (CGRectGetMinX(self.view.frame) + p.x > CGRectGetWidth(_sideBar.frame)/2) {
             [UIView animateWithDuration:0.25 animations:^{
                 self.view.frame = CGRectMake(CGRectGetWidth(_sideBar.frame), 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+                _sideBar.frame = CGRectMake(0, 0, CGRectGetWidth(_sideBar.frame), CGRectGetHeight(_sideBar.frame));
+                _shadowView.frame = CGRectMake(CGRectGetMaxX(_sideBar.frame), 0, CGRectGetWidth(_shadowView.frame), CGRectGetHeight(_shadowView.frame));
             }];
             
         }else {
             [UIView animateWithDuration:0.25 animations:^{
                 self.view.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+                _sideBar.frame = CGRectMake(-CGRectGetWidth(_sideBar.frame), 0, CGRectGetWidth(_sideBar.frame), CGRectGetHeight(_sideBar.frame));
+                _shadowView.frame = CGRectMake(CGRectGetMaxX(_sideBar.frame), 0, CGRectGetWidth(_shadowView.frame), CGRectGetHeight(_shadowView.frame));
+                _shadowView.hidden = YES;
             }];
 
         }
