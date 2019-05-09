9f3178fdf4b3a823c269423513e22287b4b31264
zjl <369907659@qq.com>
2019-05-09 19:26:54 +0800

    优化侧边栏滑出的方式

--- ZJLFaceProject/SideBar/SideBarViewController.m
+++ ZJLFaceProject/SideBar/SideBarViewController.m
@@ -55,8 +55,8 @@
     CGPoint p = [panGes translationInView:self.view];
     NSLog(@"%f",p.x);
     if (panGes.state == 2) {
-        if (CGRectGetMinX(self.view.frame) + p.x >= 0 && CGRectGetMinX(self.view.frame) + p.x <= CGRectGetWidth(_sideBar.frame)) {
-            self.view.frame = CGRectMake(CGRectGetMinX(self.view.frame) + p.x, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+        if (CGRectGetMinX(self.navigationController.view.frame) + p.x >= 0 && CGRectGetMinX(self.navigationController.view.frame) + p.x <= CGRectGetWidth(_sideBar.frame)) {
+            self.navigationController.view.frame = CGRectMake(CGRectGetMinX(self.navigationController.view.frame) + p.x, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
             [panGes setTranslation:CGPointZero inView:self.view];
             _sideBar.frame = CGRectMake(CGRectGetMinX(_sideBar.frame) + p.x, 0, CGRectGetWidth(_sideBar.frame), CGRectGetHeight(_sideBar.frame));
             _shadowView.hidden = NO;
@@ -64,16 +64,16 @@
         }
 
     }else if (panGes.state == 3){
-        if (CGRectGetMinX(self.view.frame) + p.x > CGRectGetWidth(_sideBar.frame)/2) {
+        if (CGRectGetMinX(self.navigationController.view.frame) + p.x > CGRectGetWidth(_sideBar.frame)/2) {
             [UIView animateWithDuration:0.25 animations:^{
-                self.view.frame = CGRectMake(CGRectGetWidth(_sideBar.frame), 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+                self.navigationController.view.frame = CGRectMake(CGRectGetWidth(_sideBar.frame), 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
                 _sideBar.frame = CGRectMake(0, 0, CGRectGetWidth(_sideBar.frame), CGRectGetHeight(_sideBar.frame));
                 _shadowView.frame = CGRectMake(CGRectGetMaxX(_sideBar.frame), 0, CGRectGetWidth(_shadowView.frame), CGRectGetHeight(_shadowView.frame));
             }];
             
         }else {
             [UIView animateWithDuration:0.25 animations:^{
-                self.view.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+                self.navigationController.view.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
                 _sideBar.frame = CGRectMake(-CGRectGetWidth(_sideBar.frame), 0, CGRectGetWidth(_sideBar.frame), CGRectGetHeight(_sideBar.frame));
                 _shadowView.frame = CGRectMake(CGRectGetMaxX(_sideBar.frame), 0, CGRectGetWidth(_shadowView.frame), CGRectGetHeight(_shadowView.frame));
                 _shadowView.hidden = YES;
